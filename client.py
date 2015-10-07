411d7e36eb2d91a26f8e62f8d727dcdc634022b6
kgover <kgover@xtreme-eda.com>
2015-10-06 22:49:03 -0700

    Update client.py
    Added packet ID check to prevent replay attacks.

--- client.py
+++ client.py
@@ -15,19 +15,22 @@
 from Crypto.Hash import HMAC
 from Crypto.Cipher import AES
 from Crypto import Random
-import binascii
-import socket
+import binascii, socket, sys, datetime
+
+# Get the command to be sent from the command line.
+msg_file = sys.argv[1]
 
 # ----------------------------------------------------------------------------
 #  Read in the message we want to transmit and the bank.auth file containing
-#  the 2 128-bit encryption keys.
+#  the 2 128-bit encryption keys.  The keys are in hexadecimal and need to be
+#  converted back to binary.
 # ----------------------------------------------------------------------------
 try:
-    fi = open('./message.txt', 'r')
+    fi = open(msg_file, 'r')
     p_msg = fi.read()
     fi.close()
 except IOError:
-    print('Cannot find file: message.txt')
+    print('Cannot find file: ' + msg_file)
     exit(255)
 
 try:
@@ -44,34 +47,33 @@ print '\n' + p_msg + '\n'
 #  We are using "encrypt then authenticate" since this provides CCA security.
 #  This code does the following:
 #
-#      * Breaks the contents of the bank.auth file into 2 128-bit keys.
+#      * Break the contents of the bank.auth file into 2 128-bit keys.
 #
-#      * Does the encryption of the message using an AES block cipher in
-#        cipher feedback mode (CFB).
+#      * Do the encryption of the message using an AES block cipher in cipher
+#        feedback mode (CFB).  The message includes a 26-byte datetime stamp
+#        that the bank can use as a packet ID to prevent replay attacks.
 #
 #      * Takes the encrypted message and runs it through an HMAC hash function
 #        so we can be certain it has not been altered in transit.
 #
-#      * Create packet for transmission in hexadecimal form.
+#      * Create packet for transmission in hexadecimal form.  The hash tag is
+#        already in hex but the ciphertext needs to be converted.
 #
 #      * Sends the packet out on the port.
 #
-#  While this does provide CCA security, we still need to worry about replay,
-#  re-ordering and reflection attacks. This can be made more secure by
-#  including packet numbers that are tracked on each side.
 # ----------------------------------------------------------------------------
 key_enc = k_tmp[0:AES.block_size]
 key_mac = k_tmp[AES.block_size:]
 
 iv = Random.new().read(AES.block_size)
 cipher = AES.new(key_enc, AES.MODE_CFB, iv)
-c_msg = iv + cipher.encrypt(p_msg)
+c_msg = iv + cipher.encrypt(p_msg + str(datetime.datetime.now())) 
 
 hash = HMAC.new(key_mac)
 hash.update(c_msg)
 
-packet = hash.hexdigest() + binascii.hexlify(c_msg)
+pkt = hash.hexdigest() + binascii.hexlify(c_msg)
 
 channel = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 channel.connect(('localhost', 3000))
-channel.send(packet)
+channel.send(pkt)
