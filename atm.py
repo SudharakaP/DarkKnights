af74547ea34ef8171e083a0ddcd530c27f83ee1c
Sudharaka Palamakumbura <sudharakap@mun.ca>
2015-10-08 07:29:46 -0230

    Separated Helper Functions.

--- atm.py
+++ atm.py
@@ -7,7 +7,6 @@
 import sys
 import os
 import json
-import re
 import binascii
 import socket
 import datetime
@@ -16,66 +15,7 @@ from Crypto.Hash import HMAC
 from Crypto.Cipher import AES
 from Crypto import Random
 from hmac import compare_digest
-
-
-#--------------------------------------------------------------------------------------------------------------------
-#Helper functions: These might better located in a separate module as the bank may have to do some validation as well
-#--------------------------------------------------------------------------------------------------------------------
-
-def is_valid_amount_format(amount, max_amount=4294967295.99):
-
-    """Balances and currency amounts are specified as a number indicating a whole amount and a fractional input separated by a period.
-    The fractional input is in decimal and is always two digits and thus can include a leading 0 (should match /[0-9]{2}/).
-    The interpretation of the fractional amount v is that of having value equal to v/100 of a whole amount 
-    (akin to cents and dollars in US currency).
-    Command line input amounts are bounded from 0.00 to 4294967295.99 inclusively
-    but an account may accrue any non-negative balance over multiple transactions."""
-
-    pattern = re.compile(r'^(0|[1-9][0-9]*)(\.[0-9]{2})?$')
-    if not re.match(pattern, amount) or float(amount) > max_amount:
-        return False
-    return True
-
-
-def is_valid_account_format(account):
-
-    """Account names are restricted to same characters as file names
-    but they are inclusively between 1 and 250 characters of length, and "." and ".." are valid account names."""
-
-    pattern = re.compile(r'^[_\-\.0-9a-z]{1,250}$')
-    if not re.match(pattern, account):
-        return False
-    return True
-
-def is_valid_filename_format(file_name):
-
-    """File names are restricted to underscores, hyphens, dots, digits, and lowercase alphabetical characters 
-    (each character should match /[_\-\.0-9a-z]/).
-     File names are to be between 1 and 255 characters long. The special file names "." and ".." are not allowed."""
-
-    if file_name in ['.', '..']:
-        return False
-
-    pattern = re.compile(r'^[_\-\.0-9a-z]{1,255}$')
-    if not re.match(pattern, file_name):
-        return False
-    return True
-
-
-def is_valid_ip_address(ip_address):
-
-    """Validate accordign to spec: 
-    i.e., four numbers between 0 and 255 separated by periods."""
-
-    pattern = re.compile(r'(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})')
-    match = re.match(pattern, ip_address)
-    if not match or not match.groups():
-        return False
-    valid_numbers = [True for group in match.groups() if 0 <= int(group) <= 255]
-    if valid_numbers.count(True) != 4:
-        return False
-    return True
-
+from helper import *
 
 #---------------------
 #Parse ATM CLI options
@@ -89,7 +29,7 @@ class ATMOptionParser(OptionParser):
         sys.exit(255)
 
 #-----------------------------------------------------------------------------------------
-#ATM class that offloads work to Bank server, which will return 255 or JSON-encoded string
+# ATM class that offloads work to Bank server, which will return 255 or JSON-encoded string
 #-----------------------------------------------------------------------------------------
 
 
@@ -244,7 +184,7 @@ def main():
     (options, args) = parser.parse_args()
 
     #-----------------------
-    #Basic input validation
+    # Basic input validation
     #-----------------------
 
     #Check that length of string arguments is not over 4096 characters
@@ -252,7 +192,7 @@ def main():
         if isinstance(option, str) and len(option) > 4096:
             parser.error('Argument too long for one of the options.')
 
-    #Check that required parameter is passed
+    # Check that required parameter is passed
     if not options.account:
         parser.error('"-a" is required.')
 
